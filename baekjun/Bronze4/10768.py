5c2cc081b4621b7b0b676783191c5070c9c540ea
ultting <hard7762@naver.com>
2022-06-10 00:41:36 +0900

    com

--- baekjun/Bronze4/10768.py
+++ baekjun/Bronze4/10768.py
@@ -3,9 +3,14 @@
 month = int(input())
 daily = int(input())
 
-if month > 2 and daily > 18:
+if month > 2:
     print("After")
-elif month < 2 and daily < 18 :
+elif month < 2 :
     print("Before")
-elif  month == 2 and daily == 18:
-    print("Special")
+elif month == 2 :
+    if daily == 18:
+        print("Special")
+    elif daily > 18:
+        print("After")
+    elif daily < 18:
+        print("Before")
